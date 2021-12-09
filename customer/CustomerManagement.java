8cf6fd6adb36acaa8311d8292ef5f92ae334f798
ryoshi007 <74929046+ryoshi007@users.noreply.github.com>
2021-12-09 18:08:20 +0800

    Update CustomerManagement.java
    Changes in naming of methods and parameters

--- customer/CustomerManagement.java
+++ customer/CustomerManagement.java
@@ -23,37 +23,37 @@ public class CustomerManagement {
     
     public void listName() {
         for (Customer customer : this.customerList) {
-            System.out.println(customer.getusername());
+            System.out.println(customer.getUsername());
         }
     }
     
-    public void addContentToCustomerFile(String input, String customerusername) {
-        String fileName = this.checkFileName(customerusername);
+    public void addContentToCustomerFile(String input, String customerUsername) {
+        String fileName = this.checkFileName(customerUsername);
         if (fileName != null) {
             this.customerData.addContentToFile(fileName, input);
         }
     }
     
-    public void deleteContentFromProductFile(String input, String customerusername) {
-        String fileName = this.checkFileName(customerusername);
+    public void deleteContentFromCustomerFile(String input, String customerUsername) {
+        String fileName = this.checkFileName(customerUsername);
         if (fileName != null) {
             this.customerData.deleteContentFromFile(fileName, input);
         }
     }
     
-    public void changeDataInProductFile(String oldInput, String newInput, String customerusername) {
-        String fileName = this.checkFileName(customerusername);
+    public void changeDataInCustomerFile(String oldInput, String newInput, String customerUsername) {
+        String fileName = this.checkFileName(customerUsername);
         if (fileName != null) {
             this.customerData.changeContentInFile(fileName, oldInput, newInput);
         }
     }
     
-    public void deleteCustomer(String customerusername) {
-        String fileName = this.checkFileName(customerusername);
+    public void deleteCustomer(String customerUsername) {
+        String fileName = this.checkFileName(customerUsername);
         this.customerData.deleteFile(fileName);
     }
     
-    private String checkFileName(String customerusername) {
+    private String checkFileName(String customerUsername) {
         List<String> fileList = this.customerData.returnAllFile();
         String possibleFileName = "";
         double existingSimilarityPoint = 0;
