64d6c45c245e76aca011cdba7085da24bb2a6ad5
ryoshi007 <74929046+ryoshi007@users.noreply.github.com>
2021-12-10 21:12:38 +0800

    Omazon v2
    Integrate with Customer class

--- Interface/Omazon.java
+++ Interface/Omazon.java
@@ -1,12 +1,23 @@
 package Interface;
 
+import customer.Customer;
+import customer.CustomerDatabase;
+import customer.CustomerList;
 import java.util.Scanner;
 
 public class Omazon {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        new Homepage(scanner);
+        
+        //Create customer from info in text file
+        CustomerList customerList = new CustomerDatabase().loadFile();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        Customer customer = customerList.searchCustomer(email);
+        
+        
+        new Homepage(scanner, customer);
     }
     
 }
