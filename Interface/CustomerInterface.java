13a1c52f3af9aed27f9e7d9ef71687b94c61439a
ryoshi007 <74929046+ryoshi007@users.noreply.github.com>
2021-12-14 00:37:02 +0800

    Update CustomerInterface.java

--- Interface/CustomerInterface.java
+++ Interface/CustomerInterface.java
@@ -72,7 +72,7 @@ public class CustomerInterface {
         String oldUsername = this.customer.setUsername();
         String oldInput = "Username;" + oldUsername;
         String newInput = "Username;" + this.customer.getUsername();
-        this.customerManagement.changeDataInCustomerFile(oldInput, newInput, oldUsername);
+        this.customerManagement.changeDataInCustomerFile(oldInput, newInput, this.customer.getID());
         new CustomerInterface(scanner, this.customer, this.customerManagement);
     }
     
@@ -80,7 +80,7 @@ public class CustomerInterface {
         String oldPassword = this.customer.setPassword();
         String oldInput = "Password;" + oldPassword;
         String newInput = "Password;" + this.customer.getPassword();
-        this.customerManagement.changeDataInCustomerFile(oldInput, newInput, this.customer.getUsername());
+        this.customerManagement.changeDataInCustomerFile(oldInput, newInput, this.customer.getID());
         new CustomerInterface(scanner, this.customer, this.customerManagement);
     }
     
@@ -88,7 +88,7 @@ public class CustomerInterface {
         String oldEmail = this.customer.setEmail();
         String oldInput = "Email;" + oldEmail;
         String newInput = "Email;" + this.customer.getEmail();
-        this.customerManagement.changeDataInCustomerFile(oldInput, newInput, this.customer.getUsername());
+        this.customerManagement.changeDataInCustomerFile(oldInput, newInput, this.customer.getID());
         new CustomerInterface(scanner, this.customer, this.customerManagement);
     }
     
@@ -96,7 +96,7 @@ public class CustomerInterface {
         String oldAddress = this.customer.setAddress();
         String oldInput = "Address;" + oldAddress;
         String newInput = "Address;" + this.customer.getAddress();
-        this.customerManagement.changeDataInCustomerFile(oldInput, newInput, this.customer.getUsername());
+        this.customerManagement.changeDataInCustomerFile(oldInput, newInput, this.customer.getID());
         new CustomerInterface(scanner, this.customer, this.customerManagement);
     }
     
@@ -104,11 +104,11 @@ public class CustomerInterface {
         String oldPaymentPassword = this.customer.setPaymentPassword();
         String oldInput = "PaymentPassword;" + oldPaymentPassword;
         String newInput = "PaymentPassword;" + this.customer.getPaymentPassword();
-        this.customerManagement.changeDataInCustomerFile(oldInput, newInput, this.customer.getUsername());
+        this.customerManagement.changeDataInCustomerFile(oldInput, newInput, this.customer.getID());
         new CustomerInterface(scanner, this.customer, this.customerManagement);
     }
     
-    private void topUpBalance(){       
+    private void topUpBalance(){
         double oldBalance = this.customer.addBalance();
         String oldBalanceInString, newBalanceInString;
         if (oldBalance % 1 == 0) {
@@ -126,7 +126,7 @@ public class CustomerInterface {
         
         oldBalanceInString = "Balance;" + oldBalanceInString;
         newBalanceInString = "Balance;" + newBalanceInString;
-        this.customerManagement.changeDataInCustomerFile(oldBalanceInString, newBalanceInString, this.customer.getUsername());
+        this.customerManagement.changeDataInCustomerFile(oldBalanceInString, newBalanceInString, this.customer.getID());
         new CustomerInterface(scanner, this.customer, this.customerManagement);
     }
     
@@ -134,13 +134,13 @@ public class CustomerInterface {
         clearScreen();
         System.out.println("---------------------------- Order History ----------------------------");
         int count = 0;
-        ArrayList<Product> products = this.customer.getOrderHistory();
+        ArrayList<String> products = this.customer.getOrderHistory();
         if (products.isEmpty()) {
             System.out.println("You haven't purchase a product");
         } else {
-            for (Product product : products) {
+            for (String productName : products) {
                 ++count;
-                System.out.println("No. " + count + " - " + product.getName());
+                System.out.println("No. " + count + " - " + productName);
             }
         }
 
@@ -148,11 +148,8 @@ public class CustomerInterface {
         System.out.println("A. Back to user settings");
         System.out.println("B. Back to homepage");
         System.out.println();
-        if (products.isEmpty()) {
-            System.out.print("What to do next? (A/B): " + TEXT_YELLOW); 
-        } else {
-            System.out.print("What to do next? (1-" + count + ") / (A/B): " + TEXT_YELLOW);
-        }
+        System.out.print("What to do next? (A/B): " + TEXT_YELLOW); 
+
         String input = scanner.nextLine();
         System.out.print(TEXT_RESET);
 
@@ -160,10 +157,7 @@ public class CustomerInterface {
             new CustomerInterface(scanner, customer, customerManagement);
         } else if (input.equals("B")) {
             new Homepage(this.scanner, customer);
-        } else {
-            int index = Integer.valueOf(input) - 1;
-            new ProductInterface(this.scanner, customer, products.get(index), new ProductManagement());
-        }
+        } 
     }
     
     private void favouriteProduct() {
@@ -215,3 +209,4 @@ public class CustomerInterface {
         }
     }
 }
+
