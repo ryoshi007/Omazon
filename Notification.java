23c79b54704c359e51e28ce3e2d603e3e389ff86
ryoshi007 <74929046+ryoshi007@users.noreply.github.com>
2022-01-16 05:03:32 +0800

    Update Notification.java

--- Notification.java
+++ Notification.java
@@ -1,16 +1,22 @@
 package omazon;
 
+import java.util.Date;
+
 public class Notification {
     private String productID;
     private String customerID;
     private int amount;
     private int index;
+    private Date currentDate;
+    private double payAmount;
     
-    public Notification(String productID, String customerID, int amount, int index) {
+    public Notification(String productID, String customerID, int amount, int index, Date currentDate, double payAmount) {
         this.productID = productID;
         this.customerID = customerID;
         this.amount = amount;
         this.index = index;
+        this.currentDate = currentDate;
+        this.payAmount = payAmount;
     }
     
     public String getProductID() {
@@ -27,5 +33,13 @@ public class Notification {
     
     public int getIndex() {
         return this.index;
+    }
+    
+    public Date getCurrentDate() {
+        return this.currentDate;
+    }
+    
+    public double getPayAmount() {
+        return this.payAmount;
     }
 }
