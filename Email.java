021b6e884b1c548a37c018ed1304e02b23cfecb6
ryoshi007 <74929046+ryoshi007@users.noreply.github.com>
2022-01-16 17:30:13 +0800

    Update Email.java

--- Email.java
+++ Email.java
@@ -1,45 +1,77 @@
 package omazon;
 
 import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.Session;
 import javax.mail.Authenticator;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
         
 public class Email {
 
-    public void sendEmail(String recipient, String content) throws MessagingException {
+    public static void sendEmail(String recipient, String content, String imagePath) {
+        String from = "omazonhelpdesk@gmail.com";
+
+        final String password = "omazon123";
+
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", "true");
         properties.put("mail.smtp.starttls.enable", "true");
         properties.put("mail.smtp.host", "smtp.gmail.com");
         properties.put("mail.smtp.port", "587");
-        
-        String email = "omazonhelpdesk@gmail.com";
-        String password = "omazon123";
-        
+
         Session session = Session.getInstance(properties, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(email, password);
+                return new PasswordAuthentication(from, password);
             }
         });
 
-        Message message = prepareMessage(session, email, recipient, content);
-        
-        Transport.send(message);   
+        try {
+           Message message = new MimeMessage(session);
+           message.setFrom(new InternetAddress(from));
+           message.setRecipients(Message.RecipientType.TO,
+              InternetAddress.parse(recipient));
+
+           message.setSubject("Omazon Order");
+           BodyPart messageBodyPart = new MimeBodyPart();
+           messageBodyPart.setContent(content, "text/html");
+           Multipart multipart = new MimeMultipart();
+           multipart.addBodyPart(messageBodyPart);
+
+
+           messageBodyPart = new MimeBodyPart();
+           String filename = imagePath;
+           DataSource source = new FileDataSource(filename);
+           messageBodyPart.setDataHandler(new DataHandler(source));
+           messageBodyPart.setFileName(filename);
+           multipart.addBodyPart(messageBodyPart);
+           message.setContent(multipart);
+
+           Transport.send(message);
+
+           System.out.println("Sent message successfully....");
+
+        } catch (MessagingException e) {
+           throw new RuntimeException(e);
+        }
     }
     
     public String prepareBuyerContent(String productName, String purchaseAmount, double purchasePrice) {
-        String header = "<h1> Your order has been confirmed! </h1>";
+        String header = "<h1> Your order has been confirmed! </h1>";    
         String name = "<h2> Product Name - " + productName + "</h2>";
-        String amount = "<h3> Purchase Amount - RM " + purchaseAmount + "</h3>";
-        String paid = "<h3> Total Price - " + purchasePrice + "</h3>";
+        String amount = "<h3> Purchase Amount - " + purchaseAmount + "</h3>";
+        String paid = "<h3> Total Price - RM " + purchasePrice + "</h3>";
         String ending = "<h5> Keep this email as a proof of purchase! </h5>";
         
         String content = header + "\n" + name + "\n" + amount + "\n" + paid + "\n" + ending;
@@ -50,7 +82,7 @@ public class Email {
         String header = "<h1> An order has been made! </h1>";
         String name = "<h2> Product Name - " + productName + "</h2>";
         String amount = "<h3> Purchase Amount - " + purchaseAmount + "</h3>";
-        String paid = "<h3> Total Price - " + purchasePrice + "</h3>";
+        String paid = "<h3> Total Price - RM " + purchasePrice + "</h3>";
         String info = "<h2> Buyer's Info </h2>";
         String buyer = "<h3> Buyer's name: " + buyerName + "</h3>";
         String address = "<h3> Buyer's address: " + buyerAddress + "</h3>"; 
@@ -59,13 +91,5 @@ public class Email {
         String content = header + "\n" + name + "\n" + amount + "\n" + paid + "\n\n" + info + "\n" + buyer + "\n" + address + "\n" + ending;
         return content;
     }
-    
-    private static Message prepareMessage(Session session, String sender, String recipient, String content) throws AddressException, MessagingException {
-        Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress(sender));
-        message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
-        message.setSubject("Omazon Order");
-        message.setContent(content, "text/html");
-        return message;
-    }
+
 }
